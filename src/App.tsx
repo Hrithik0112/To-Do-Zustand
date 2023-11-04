fb8355814a0bef36b6364826ed92bd1fbb2c0942
Hrithik <slumdog4321@gmail.com>
2023-11-04 13:27:08 +0530

    todo form added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <h1 className="font-bold text-3xl">Hello World</h1>
+      <div className="h-screen w-screen flex justify-center items-center">
+        <div className="w-[600px] p-3 rounded-md shadow-lg">
+          <h1 className="font-bold text-3xl">To-Do's</h1>
+          <p className="text-xl">Add Your Tasks</p>
+          <form>
+            <div className=" mt-5">
+              <input
+                type="text"
+                placeholder="Enter Your Tasks"
+                className="w-full h-10 p-3 outline-black border border-gray-500 rounded-lg"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
